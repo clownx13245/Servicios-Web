acb60dff3c09f7674840a18e276be911f583298e
Juan Rios <juandavidrios12@gmail.com>
2025-10-13 14:09:25 -0500

    feat: agregar migraciones completas para productos

--- database/migrations/2025_10_12_204637_create_t_productos_table.php
+++ database/migrations/2025_10_12_204637_create_t_productos_table.php
@@ -12,8 +12,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('t_productos', function (Blueprint $table) {
-            $table->id();
+            $table->id(); // id – PK – autoincrementable
+            
+            $table->string('nombre');
+            $table->unsignedBigInteger('categoria_id'); // categoría – FK
+            $table->integer('stock'); // stock – entero
+            $table->decimal('precio', 8, 2); // precio – decimal (8 dígitos total, 2 decimales)
+            $table->enum('estado', ['A', 'I'])->default('A'); // estado – parametrizado (A/I)
+            
             $table->timestamps();
+
+            // Clave foránea para categoría
+            $table->foreign('categoria_id')->references('id')->on('t_categorias');
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Rutas para Customers
+Route::get('/customers', [CustomerController::class, 'index']);
+Route::get('/customers/{id}', [CustomerController::class, 'show']);
+
+
+
+
+/*use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+*/
